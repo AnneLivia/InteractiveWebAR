aa3c5e5edd297ca502f8377b3ae18db0f39709ec
Anne Livia <annelivia16@gmail.com>
2022-07-28 11:31:55 -0300

    updated buttons and refactor

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,20 @@
+// para inserir evento de click nos botões
 const githubButton = document.querySelector('#github-button');
 const linkedinButton = document.querySelector('#linkedin-button');
 const rightButton = document.querySelector('#right-button');
 const leftButton = document.querySelector('#left-button');
-const panel = document.querySelector('#panel');
-const sceneEl = document.querySelector('a-scene');
 
+// para fazer a troca de conteudo
+const panel = document.querySelector('#panel');
+// para pegar o evento de cena carregada
+const sceneEl = document.querySelector('a-scene');
+// para controlar o video
 const video = document.querySelector('#video');
+// para parar o video se o target não for mais encontrado
+const target = document.querySelector('#the-target');
+
+// mindar engine
+const arSystem = sceneEl.systems["mindar-image-system"];
 
 video.pause();
 
@@ -17,17 +26,18 @@ linkedinButton.addEventListener('click', () => {
   window.open('https://www.linkedin.com/in/annelivia/', '_blank');
 });
 
+// item inicial
 let currentItem = 2;
 
 panel.setAttribute('visible', true);
 
-const showPortfolioItem = (item) => {
+const showItem = (item) => {
   // vai deixar visivel o item atual e invisivel o restante
   for (let i = 0; i <= 2; i++) {
     document.querySelector('#item' + i).setAttribute('visible', i === item);
   }
 
-  // é o vídeo
+  // é o vídeo, então precisa dar play, se não, pausa
   if (item === 0) {
     document.querySelector('#anchor-video').setAttribute('visible', true);
     video.play();
@@ -37,18 +47,20 @@ const showPortfolioItem = (item) => {
   }
 };
 
+// botão para esquerda é clicado, então decrementa item atual e exibe
 leftButton.addEventListener('click', () => {
   console.log(currentItem)
   currentItem -= 1;
   if (currentItem < 0) currentItem = 2;
-  showPortfolioItem(currentItem);
+  showItem(currentItem);
 });
 
+// botão para esquerda é clicado, então incrementa item atual e exibe
 rightButton.addEventListener('click', () => {
   currentItem += 1;
   if (currentItem > 2) currentItem = 0;
-  console.log(currentItem)
-  showPortfolioItem(currentItem);
+  // console.log(currentItem)
+  showItem(currentItem);
 });
 
 // arReady event triggered when ready
@@ -58,4 +70,21 @@ sceneEl.addEventListener("arReady", (event) => {
 // arError event triggered when something went wrong. Mostly browser compatbility issue
 sceneEl.addEventListener("arError", (event) => {
    console.log("MindAR failed to start")
+});
+
+target.addEventListener("targetFound", event => {
+  if (currentItem === 0) {
+    video.play()
+  }
+  console.log("Found")
+});
+
+target.addEventListener("targetLost", event => {
+  // reiniciar o video, se retirar o target
+  if (currentItem === 0) {
+    video.pause()
+    video.currentTime = 0
+  }
+ 
+  console.log("lost")
 });
